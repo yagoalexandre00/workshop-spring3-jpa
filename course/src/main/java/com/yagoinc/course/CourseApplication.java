e98076c6839e105912a494e59d74941716a8290f
Yago Alexandre <goltarayago@gmail.com>
2023-02-27 12:11:40 -0300

    Project created

---
+++ course/src/main/java/com/yagoinc/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.yagoinc.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ course/src/test/java/com/yagoinc/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagoinc.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
