e7373fd616b1f8363081bab309b51fd1a09de22c
Yago Alexandre <goltarayago@gmail.com>
2023-02-27 12:56:32 -0300

    User entity and user resource

---
+++ course/src/main/java/com/yagoinc/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.yagoinc.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yagoinc.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Yago", "yago@mail", "162734", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+}
