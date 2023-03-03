742055a6959e5358b0bdf005ed19d1d4af748cbd
Yago Alexandre <goltarayago@gmail.com>
2023-03-03 10:35:33 -0300

    Many to many association with JoinTable

--- course/src/main/java/com/yagoinc/course/config/TestConfig.java
+++ course/src/main/java/com/yagoinc/course/config/TestConfig.java
@@ -37,6 +37,28 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		Category c1 = new Category(null, "Electronics");
+		Category c2 = new Category(null, "Books");
+		Category c3 = new Category(null, "Computers");
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(c2);
+		p2.getCategories().add(c1);
+		p2.getCategories().add(c3);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(null, "Yago Alexandre", "yago@mail", "988888888", "123456");
 		User u2 = new User(null, "Cayo Cutrim", "cayo@mail", "977777777", "123456");
 		User u3 = new User(null, "Thiago Gonzalez", "thiago@mail", "977777777", "123456");
@@ -47,19 +69,8 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2022-08-26T18:35:24.00Z"), OrderStatus.SHIPPED, u4);
 		Order o4 = new Order(null, Instant.parse("2022-08-26T18:35:24.00Z"), OrderStatus.DELIVERED, u1);
 
-		Category c1 = new Category(null, "Marketing");
-		Category c2 = new Category(null, "Sales");
-		Category c3 = new Category(null, "IT");
-
-		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
-		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
-		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
-		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
-		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 	}
 }
