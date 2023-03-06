084634e035459ae03f249f222e96c9d914692938
Yago Alexandre <goltarayago@gmail.com>
2023-03-06 12:56:08 -0300

    User insert part 1

--- course/src/main/java/com/yagoinc/course/resources/UserResource.java
+++ course/src/main/java/com/yagoinc/course/resources/UserResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,11 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
