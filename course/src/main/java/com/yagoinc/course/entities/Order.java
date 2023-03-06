77aaa6e8677b58a32cf645adaf5f311d787c855c
Yago Alexandre <goltarayago@gmail.com>
2023-03-06 09:48:43 -0300

    Subtotal and Total methods

--- course/src/main/java/com/yagoinc/course/entities/Order.java
+++ course/src/main/java/com/yagoinc/course/entities/Order.java
@@ -96,6 +96,15 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		Double total = 0.0;
+		for (OrderItem item : items) {
+			total += item.getSubTotal();
+		}
+
+		return total;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
